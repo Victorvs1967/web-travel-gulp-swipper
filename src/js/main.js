caf5f73ebd564d43ceabbdee246f86ab6c0cd64c
Victorvs1967 <victorsmirnov@me.com>
2023-09-02 15:53:41 +0300

    add popular

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,29 @@
 import autoComplete from "@tarekraafat/autocomplete.js";
+import { easepick, TimePlugin } from '@easepick/bundle';
 
-import AirDatepicker from 'air-datepicker';
-import 'air-datepicker/air-datepicker.css';
+// import AirDatepicker from 'air-datepicker';
+// import 'air-datepicker/air-datepicker.css';
 
-import mobileNav from './modules/mobile-nav.js';
+// import mobileNav from './modules/mobile-nav.js';
 import locationList from './modules/locationList.js';
 
-new AirDatepicker('#calendar', {
-  selectedDates: [new Date()],
-  isMobile: true,
-  autoClose: true,
+const picker = new easepick.create({
+  element: "#datepicker",
+  css: [
+    "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css",
+  ],
+  zIndex: 10,
+  format: " DD/MM/YYYY, HH:mm",
+  plugins: [
+    TimePlugin
+  ]
 });
+
+// new AirDatepicker('#calendar', {
+//   selectedDates: [new Date()],
+//   isMobile: true,
+//   autoClose: true,
+// });
 
 const autoCompleteJS = new autoComplete(
   {
